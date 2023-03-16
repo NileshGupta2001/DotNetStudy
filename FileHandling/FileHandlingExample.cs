279acda933e663595592887ad334477904f818e7
Nilesh <nileshgupta2001@gmail.com>
2023-03-16 09:40:00 +0530

    File Handling Added

---
+++ FileHandling/FileHandlingExample.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHandling
+{
+    internal class FileHandlingExample
+    {
+        public string FilePath = "E://Study.txt";
+        public void ReadFile()
+        {
+
+        }
+
+        public void WriteFile()
+        {
+
+            try
+            {
+
+
+                FileStream fs = new FileStream(FilePath, FileMode.Append, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(fs);
+                string sr = Console.ReadLine();
+                sw.WriteLine(sr);
+                sw.Flush();
+                sw.Close();
+                fs.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ FileHandling/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FileHandling;
+Console.WriteLine("Hello, World!");
+FileHandlingExample fhe = new FileHandlingExample();
+fhe.WriteFile();
