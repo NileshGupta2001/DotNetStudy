3d90b78bbfd99162e5db1d6aded30e943d15afe2
Nilesh <nileshgupta2001@gmail.com>
2023-03-17 08:41:13 +0530

    Update..

--- FileHandling/Program.cs
+++ FileHandling/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using FileHandling;
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 FileHandlingExample fhe = new FileHandlingExample();
 //fhe.WriteFile();
 fhe.ReadFile();
