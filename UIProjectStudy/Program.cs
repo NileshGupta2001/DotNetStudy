88477dd872e0544d0165a7ec163ed1be8ed09292
Nilesh <nileshgupta2001@gmail.com>
2023-03-31 08:39:51 +0530

    Update

---
+++ UIProjectStudy/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ADOStudy;
+
+namespace UIProjectStudy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DBContext db = new DBContext();
+           var lstrole= db.ReadData();
+            foreach (var item in lstrole)
+            {
+                Console.WriteLine(item.UserID);
+            }
+          
+        }
+    }
+}
