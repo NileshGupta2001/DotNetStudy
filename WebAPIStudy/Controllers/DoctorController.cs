476db212810064df24064af744129cce03093ebc
Nilesh <nileshgupta2001@gmail.com>
2023-05-16 08:55:30 +0530

    update neww

--- WebAPIStudy/Controllers/DoctorController.cs
+++ WebAPIStudy/Controllers/DoctorController.cs
@@ -3,6 +3,14 @@ using WebAPIStudy.Model;
 
 namespace WebAPIStudy.Controllers
 {
+    /*
+     * HTTP RESPONSE CODES/STATUS
+     * 1xx	Request received and under processing
+2xx	Successful
+3xx	Redirection, an action must be taken by either user agent (browser) or user
+4xx	Invalid Request by the client, might be because of incomplete and invalid data
+    5xx	Server Error
+     * */
     [ApiController]
     [Route("[controller]")]
     public class DoctorController : ControllerBase
@@ -12,15 +20,63 @@ namespace WebAPIStudy.Controllers
         ApplicationDBContext Db;
         
         [HttpGet(Name = "GetDoctor")]
-        public List<Doctor> Get()
+        public IActionResult Get()
         {
             lstDoctor = Db.Doctors.ToList(); 
-            return lstDoctor;
+            return Ok(lstDoctor);
+        }
+
+        [HttpPost(Name = "AddDoctor")]
+        public IActionResult Add(Doctor doctor)
+        {
+            
+            Db.Doctors.Add(doctor);
+            Db.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPatch(Name = "UpdateDoctor")]
+        public IActionResult EditDoctor(Doctor doctor)
+        {
+            var existingDoctor = Db.Doctors.FirstOrDefault(x => x.ID == doctor.ID);
+            if(existingDoctor != null)
+            {
+                existingDoctor.Name = doctor.Name;
+                existingDoctor.Speciality = doctor.Speciality;
+                Db.SaveChanges();
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+           
+            
+           
+        }
+
+        [HttpDelete(Name = "DeleteDoctor")]
+        public IActionResult DeleteDoctor(int ID)
+        {
+            var existingDoctor = Db.Doctors.FirstOrDefault(x => x.ID == ID);
+            if (existingDoctor != null)
+            {
+                Db.Doctors.Remove(existingDoctor);
+                Db.SaveChanges();
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+
+
+
         }
 
         public DoctorController(ApplicationDBContext _db)
         {
-            lstDoctor = new List<Doctor>();
+            //lstDoctor = new List<Doctor>();
             //lstDoctor.Add(new Doctor() { ID = 1, Name = "nilesh", Speciality = "Cardiologist" });
             //lstDoctor.Add(new Doctor() { ID = 2, Name = "ranveer", Speciality = "Orthologist" });
             //lstDoctor.Add(new Doctor() { ID = 3, Name = "rahul", Speciality = "Dentist" });
